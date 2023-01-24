d61ed86041550c9d4e809f695c60b6b3384805d2
yanai shohei <zeal20410026@fsgcl.jp>
2023-01-24 14:41:35 +0900

    ’/n'

--- OnsenBotTest.py
+++ OnsenBotTest.py
@@ -1712,7 +1712,7 @@ def handle_message(event):
         cur = connection.cursor()
         cur.execute('SELECT * FROM onsen_table;')
         results = cur.fetchall()
-        content_text = results[2][1].replace(" ","") + results[2][2].replace(" ","")
+        content_text = results[2][1].replace(" ","") + '/n' + results[2][2].replace(" ","")
 
 
         line_bot_api.reply_message(
